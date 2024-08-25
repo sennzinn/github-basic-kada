dd996f08f9aea258c9a3ae90ee8ff3e77a3e1a84
sennzinn <sennzinn@gmail.com>
2024-08-25 13:40:46 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+const btn =document.getElementById("btn");
+
+const text=document.getElementById("text");
+
+btn.addEventListener("click",()=>{
+  
+  //const text =document.createElement("h2");
+
+  text.textContent="ボタンをクリックしました";
+
+})
