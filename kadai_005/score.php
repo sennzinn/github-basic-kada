129c7cfa36dd8ec43bf0356e1a92b3e2340e53a2
sennzinn <sennzinn@gmail.com>
2024-09-28 16:33:29 +0900

    kadai_005をアップデート

---
+++ kadai_005/score.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  
+  <p>
+    <?php
+    $score1=80;
+    $score2=60;
+    $score3=55;
+    $score4=40;
+    $score5=100;
+    $score6=25;
+    $score7=80;
+    $score8=95;
+    $score9=30;
+    $score10=60;
+
+    $total= $score1=80
+    +$score2=60
+    +$score3=55
+    +$score4=40
+    +$score5=100
+    +$score6=25
+    +$score7=80
+    +$score8=95
+    +$score9=30
+    +$score10=60;
+
+
+    echo $average= $total / 10
+
+
+
+    ?>
+  </p>
+</body>
+
+</html>
