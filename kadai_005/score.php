2e802ca6ffdc2f76cc1d5a69ac0e0f6a0d9f2905
sennzinn <sennzinn@gmail.com>
2024-09-28 13:19:57 +0900

    kadai_005(PHP)をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  
+  <p>
+    <?php
+    $total =80+60+55+40+100+25+80+95+30+60;
+
+    echo $total / 10
+    ?>
+  </p>
+</body>
+
+</html>
